d3c7d071f090f7b8fa9f3ae58ceed1240b22cf33
Utkarsh Pancholi <utkarsh.pancholi@celebaltech.com>
2026-03-11 15:37:40 +0530

    feat: added gh-pages configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  base: "/utkarsh212.github.io/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
